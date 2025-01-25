13f8a4e9a83c5c7c04888a81b61fd674175e100d
m4gler <154061567+m4gler@users.noreply.github.com>
2025-01-25 10:19:40 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+std::vector<double> wspolczynniki(double a, double b, double c) {
+    std::vector<double> wspol;
+
+    if (a == 0) {
+        cout << "to nie funkcja kwadratowa" << endl;
+        return wspol;
+    }
+
+    double p = -b/ (2 * a);
+    double q = a * p * p + b*p + c;
+
+    wspol.push_back(p);
+    wspol.push_back(q);
+
+    cout << "wspolrzedna p to: " << p << endl;
+    cout << "wspolrzedna q to: " << q << endl;
+
+    if (a > 0) {
+        cout << "funkcja ma minimum w punkcie: " << p << " , " << q << endl;
+    }
+    else if (a < 0) {
+        cout << "funkcja ma maximum w punkcie: " << p << " , " << q << endl;
+    }
+
+    return wspol;
+}
+
+
+
+int main() {
+    double a, b, c;
+
+    cout << "Podaj wspolczynniki: ";
+    cin >> a >> b >> c;
+
+    std::vector<double> wyniki = wspolczynniki(a, b, c);
+
+    return 0;
+}
